5f824f82649b358eb8a4560fbf4ae0118ae2332d
williammiller <William234@aol.com>
2026-02-11 18:30:39 -0500

    Add reports app layout

--- src/app/pages/reports/reports.ts
+++ src/app/pages/reports/reports.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { LoggedInLayout } from "../logged-in-layout/logged-in-layout";
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.html',
   styleUrl: './reports.css',
-  standalone: true
+  standalone: true,
+  imports: [LoggedInLayout]
 })
 export class Reports {
   constructor() { }
