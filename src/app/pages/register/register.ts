81063804f98d6d0ae98e42df9ef1e9f322d65e72
williammiller <William234@aol.com>
2026-02-22 17:56:55 -0500

    Add confirm password to register form.

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthApi } from '../../services/auth-api';
+import { NgIf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, NgIf],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
   email = '';
   password = '';
+  confirmPassword = '';
   firstName = '';
   lastName = '';
   dob = '';
@@ -19,6 +21,9 @@ export class Register {
   constructor(private authApi: AuthApi, private router: Router) {}
 
   register() {
+    if (this.password !== this.confirmPassword) {
+      return;
+    }
     const dobDate = new Date(this.dob);
     this.authApi
       .register({
